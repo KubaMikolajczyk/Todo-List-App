5c91a95aa2c22fb8ef814f7f29ffc55a4ed6ba46
KubaMikolajczyk <qba600@gmail.com>
2018-08-24 09:12:02 +0200

    implement delete todo function

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,3 +1,12 @@
+// marking todo as done
 $("li").click(function(){
   $(this).toggleClass("completed");
 });
+
+// deleting todo
+$("span").click(function(event){
+  $(this).parent().fadeOut(500, function(){
+    $(this).remove();
+  })
+  event.stopPropagation();
+})
